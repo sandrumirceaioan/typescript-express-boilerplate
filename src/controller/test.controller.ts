7febcdc51ca106e3a13db1b65a780f527c71b588
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2022-03-17 16:37:55 +0200

    handlebars

--- src/controller/test.controller.ts
+++ src/controller/test.controller.ts
@@ -1,10 +1,7 @@
-import path from 'path';
-
-
 const testController = (req, res, next) => {
     try {
         console.log('TEST CONTROLLER');
-        res.sendFile(path.join(__dirname, '../views', 'test.html'));
+        res.render('index', { title:'Netflix Clones', email:'' });
     } catch (error) {
         console.log(error);
         res.status(500).send('TEST ERROR');
